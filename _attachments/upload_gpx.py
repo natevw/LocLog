d50be1c9d309d4b141379865953a5521848852ce
Nathan Vander Wilt <nate@calftrail.com>
2011-09-21 21:55:31 -0700

    add multiple file support

--- _attachments/upload_gpx.py
+++ _attachments/upload_gpx.py
@@ -19,24 +19,25 @@ def _transport(method, path, data=None):
     return json.loads(resp.read())
 
 
-file = sys.argv[1]
+files = sys.argv[1:]
 
-tree = ET.parse(file)
-for seg in tree.findall(".//{http://www.topografix.com/GPX/1/1}trkseg"):
-    seg_doc = {
-        'com.stemstorage.loclog.track': True,
-        'points': []
-    }
-    for pt in seg.findall("{http://www.topografix.com/GPX/1/1}trkpt"):
-        point = {
-             'lat': float(pt.attrib['lat']),
-             'lon': float(pt.attrib['lon'])
+for file in files:
+    tree = ET.parse(file)
+    for seg in tree.findall(".//{http://www.topografix.com/GPX/1/1}trkseg"):
+        seg_doc = {
+            'com.stemstorage.loclog.track': True,
+            'points': []
         }
-        ele = pt.find('{http://www.topografix.com/GPX/1/1}ele')
-        if ele is not None:
-            point['ele'] = float(ele.text)
-        time = pt.find('{http://www.topografix.com/GPX/1/1}time')
-        if time is not None:
-            point['time'] = time.text
-        seg_doc['points'].append(point)
-    _transport('PUT', "/loctest/trk-%s" % uuid4().hex, seg_doc)
+        for pt in seg.findall("{http://www.topografix.com/GPX/1/1}trkpt"):
+            point = {
+                 'lat': float(pt.attrib['lat']),
+                 'lon': float(pt.attrib['lon'])
+            }
+            ele = pt.find('{http://www.topografix.com/GPX/1/1}ele')
+            if ele is not None:
+                point['ele'] = float(ele.text)
+            time = pt.find('{http://www.topografix.com/GPX/1/1}time')
+            if time is not None:
+                point['time'] = time.text
+            seg_doc['points'].append(point)
+        _transport('PUT', "/loctest/loc_seg-%s" % uuid4().hex, seg_doc)

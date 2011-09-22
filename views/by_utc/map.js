2cdf429c1452365f3d50724037adcca9b1e79a60
Nathan Vander Wilt <nate@calftrail.com>
2011-09-21 21:56:08 -0700

    simple timestamp->lat/lon view for tracks

---
+++ views/by_utc/map.js
@@ -0,0 +1,11 @@
+function (doc) {
+    var exports = {};
+    // !code lib/date.js
+    var dt = exports;
+    
+    if (doc['com.stemstorage.loclog.track']) {
+        doc.points.forEach(function (pt) {
+            emit(dt.toUTCComponents(dt.newDate(pt.time)), {lat:pt.lat, lon:pt.lon});
+        });
+    }
+}

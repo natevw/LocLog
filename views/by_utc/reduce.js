bc4618032202c695b6d9871541e64074d57e3220
Nathan Vander Wilt <nate@calftrail.com>
2011-09-27 23:44:58 -0700

    experimenting with point-nearest-to-centroid reduction

--- views/by_utc/reduce.js
+++ views/by_utc/reduce.js
@@ -1,4 +1,22 @@
 function (keys, values) {
-    var i = Math.floor(values.length/2);
-    return values[i];
+    var x = 0, y = 0;
+    values.forEach(function (coord) {
+        x += coord.lon;
+        y += coord.lat;
+    });
+    x /= values.length;
+    y /= values.length;
+    
+    var minIdx, minDistSqd;
+    values.forEach(function (coord, idx) {
+        var dX = coord.lon - x,
+            dY = coord.lat - y,
+            distSqd = dX*dX + dY*dY;
+        if (!minIdx || distSqd < minDistSqd) {
+            minIdx = idx;
+            minDistSqd = distSqd;
+        }
+    });
+    
+    return values[minIdx];
 }

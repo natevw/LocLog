40529574e9361be9673ce546d6acf1c2c32cbf6d
Nathan Vander Wilt <nate@calftrail.com>
2011-09-29 23:29:02 -0700

    trying out weighted average

--- views/by_utc/reduce.js
+++ views/by_utc/reduce.js
@@ -1,22 +1,13 @@
 function (keys, values) {
-    var x = 0, y = 0;
+    var x = 0, y = 0, z = 0, n = 0;
     values.forEach(function (coord) {
-        x += coord.lon;
-        y += coord.lat;
+        x += coord.lon * (coord.n || 1);
+        y += coord.lat * (coord.n || 1);
+        z += coord.ele * (coord.n || 1);
+        n += (coord.n || 1);
     });
-    x /= values.length;
-    y /= values.length;
-    
-    var minIdx, minDistSqd;
-    values.forEach(function (coord, idx) {
-        var dX = coord.lon - x,
-            dY = coord.lat - y,
-            distSqd = dX*dX + dY*dY;
-        if (!minIdx || distSqd < minDistSqd) {
-            minIdx = idx;
-            minDistSqd = distSqd;
-        }
-    });
-    
-    return values[minIdx];
+    x /= n;
+    y /= n;
+    z /= n;
+    return {lat:y, lon:x, ele:z, n:n};
 }

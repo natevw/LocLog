db00b03749f0167cf0367d57206125b33a2d9f1e
Nathan Vander Wilt <nate@calftrail.com>
2011-10-03 22:44:37 -0700

    reduce to log(n) points by picking "interesting" points inspired by m=log(n) twist on line segment–less Douglas-Peucker

--- views/by_utc/reduce.js
+++ views/by_utc/reduce.js
@@ -1,13 +1,29 @@
-function (keys, values) {
-    var x = 0, y = 0, z = 0, n = 0;
-    values.forEach(function (coord) {
-        x += coord.lon * (coord.n || 1);
-        y += coord.lat * (coord.n || 1);
-        z += coord.ele * (coord.n || 1);
-        n += (coord.n || 1);
-    });
-    x /= n;
-    y /= n;
-    z /= n;
-    return {lat:y, lon:x, ele:z, n:n};
+function (keys, values, rereduce) {
+    if (rereduce) {
+        values = [].concat.apply([], values);
+    }
+    var maxAllowed = ~~Math.log(values.length) + 1,
+        select = [], avg = {lat:0, lon:0, n:0};
+    
+    if (values.length < maxAllowed) {
+        return values;
+    } else while (avg.n < maxAllowed) {
+        var target = (avg.n) ? {lat:avg.lat/avg.n, lon:avg.lon/avg.n} : avg,
+            maxDistSqd = -1, maxIdx;
+        values.forEach(function (coord, idx) {
+            if (select[idx]) return;
+            var dx = coord.lon - target.lon,
+                dy = coord.lat - target.lat,
+                distSqd = dx*dx + dy*dy;
+            if (distSqd > maxDistSqd) {
+                maxDistSqd = distSqd;
+                maxIdx = idx;
+            }
+        });
+        select[maxIdx] = true;
+        avg.lat += values[maxIdx].lat;
+        avg.lon += values[maxIdx].lon;
+        avg.n += 1;
+    }
+    return values.filter(function (d, idx) { return select[idx]; });
 }

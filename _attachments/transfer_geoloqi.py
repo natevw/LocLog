c869c7388192c96fdbe003dc490bb16b34611cf9
Nathan Vander Wilt <nate@calftrail.com>
2011-12-29 22:55:09 -0600

    fetch based on latest day's Geoloqi document, append points when appropriate

--- _attachments/transfer_geoloqi.py
+++ _attachments/transfer_geoloqi.py
@@ -1,5 +1,5 @@
-ACCESS_TOKEN = ""       # get from https://developers.geoloqi.com/getting-started when logged in
-FILE = "/Users/natevw/Desktop/geoloqi_dump.json"
+ACCESS_TOKEN = ""   # get from https://developers.geoloqi.com/getting-started
+DATABASE = "http://localhost:5984/loctest"
 LOQITYPE = 'com.stemstorage.loclog.geoloqi'
 
 from helpers import logging
@@ -23,30 +23,36 @@ def _transport(method, url, data=None):
     return json.loads(resp.read())
 
 
+initial = _transport('GET', DATABASE + "/_design/loclog/_view/last_geoloqi?descending=true&limit=1&include_docs=true")
+if initial['rows']:
+    start_doc = initial['rows'][0]['doc']
+    start_date = start_doc['data'][-1]['date']
+else:
+    start_doc = None
+    start_date = '2010-04-01T00:00:00Z'
 
-try:
-    fp = open(FILE, 'rb')
-    points = json.load(fp)
-    start_date = points[-1]['date']
-except IOError:
-    points = []
-    start_date = "2010-01-01T00:00:00Z"
-
+points = []
 while 1:
     url = "https://api.geoloqi.com/1/location/history?after=%s&sort=asc&ignore_gaps=1&count=1000&oauth_token=%s" % (start_date.replace("+", "%2B"), ACCESS_TOKEN)
     data = _transport('GET', url)
     if not data['points']:
+        logging.info("No more points available.")
         break
     points.extend(data['points'])
     start_date = data['points'][-1]['date']
-    logging.info("Loaded points up to %s", start_date)
+    logging.info("Loaded points up to %s.", start_date)
 
 from itertools import groupby
 points.sort(key=lambda p: p['date'])
 for k, g in groupby(points, lambda p: p['date'].split('T')[0]):
-    id, doc = "geoloqi-%s" %k.replace('-',''), {LOQITYPE:True, 'data':list(g)}
-    _transport('PUT', "http://localhost:5984/loctest/%s" % id, doc)
+    if start_doc and start_date.split('T')[0] == k:
+        start_doc['data'].extend(g)
+        id, doc = start_doc['_id'], start_doc
+        logging.info("Updating %s document.", k)
+    else:
+        id, doc = "geoloqi-%s" %k.replace('-',''), {LOQITYPE:True, 'data':list(g)}
+        logging.info("Storing %s document.", k)
+    _transport('PUT', DATABASE + "/%s" % id, doc)
+    start_doc = None
 
-fp = open(FILE, 'wb')
-json.dump(points, fp, indent=4)
-fp.close()
+logging.info("Stored %s new point(s) from Geoloqi.", len(points))

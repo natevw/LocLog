c7626d44ee08585d159a22a7f9f0e81b84f69748
Nathan Vander Wilt <nate@calftrail.com>
2012-04-12 22:30:27 -0700

    start plotting iOS app for tracklogging

---
+++ _attachments/Trailer/Trailer/TLViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLViewController.h
+//  Trailer
+//
+//  Created by Nathan Vander Wilt on 4/12/12.
+//  Copyright (c) 2012 &yet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLViewController : UIViewController
+
+@end
---
+++ _attachments/Trailer/Trailer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Trailer
+//
+//  Created by Nathan Vander Wilt on 4/12/12.
+//  Copyright (c) 2012 &yet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
+    }
+}
